80cbf6f36ee2ab380d86c3280aabbecc36c0e246
Elder Massahiro Yoshida <emy2@duke.edu>
2015-04-23 21:52:08 -0400

    Connect instead of query

--- src/Constants.h
+++ src/Constants.h
@@ -54,7 +54,7 @@ namespace Constants {
 	
 	namespace PMessages {
 		const int JOIN = 1;
-		const int QUERY = 2;
+		const int CONNECT = 2;
 		const int RESPONSE = 3;
 		const int OK = 4;
 		const int UPDATE = 5;
